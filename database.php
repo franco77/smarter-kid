9caea451557cdacf74ab45f38ca51923f6f76249
Rachel <72714577+residentevil35@users.noreply.github.com>
2020-10-11 19:11:29 -0500

    Create database.php
    This is the file used to login to the database to pull questions for the quiz.

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php 
+/* database.php 
+   Database file for final 
+ */
+   
+$servername="localhost";
+$username="root";
+$password="";
+
+$db="coughanour_final";
+
+$con=mysqli_connect($servername, $username, $password, $db);
+
+?>
